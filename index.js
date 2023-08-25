945c858f98f4f1eebd07da5705089bca71877a81
RidgeA <antongriadchenko@gmail.com>
2023-08-25 23:58:00 +0300

    parse command line arguments

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('The Weather CLI App');
+import {parseArgs} from "node:util";
+
+console.log("The Weather CLI App");
+
+const result = parseArgs({
+  args: process.argv.slice(2),
+  options: {
+    city: {
+      type: "string",
+      short: "c",
+    },
+    lat: {
+      type: "string",
+    },
+    long: {
+      type: "string",
+    },
+  },
+  allowPositionals: true,
+})
+
+console.log(result)
