f364ccfcecdd2527c16107a18f6092414cd963d5
RidgeA <antongriadchenko@gmail.com>
2023-08-28 00:21:17 +0300

    add tests for parseCommand function

--- parse-command.test.js
+++ parse-command.test.js
@@ -1,5 +1,30 @@
-import test from "node:test";
+import test, { describe } from "node:test";
+import { parseCommand } from "./parse-command.js";
+import assert from "node:assert/strict";
 
-test("hello world", () => {
-    console.log("hello world");
+describe("parseCommand()", () => {
+  test("should return result if 1 positional argument is passed", () => {
+    const args = ["current", "--city", "Vinnytsia"];
+
+    const result = parseCommand(args);
+
+    assert.equal(result.command, "current");
+    assert.equal(result.options.city, "Vinnytsia");
+  });
+
+  test("should an error 0 positional arguments are passed", () => {
+    const args = ["--city", "Vinnytsia"];
+
+    assert.throws(() => parseCommand(args), {
+      message: "Wrong number of positional arguments: 0",
+    });
+  });
+
+  test("should an error 2 positional arguments are passed", () => {
+    const args = ["current", "weather"];
+
+    assert.throws(() => parseCommand(args), {
+      message: "Wrong number of positional arguments: 2",
+    });
+  });
 });
