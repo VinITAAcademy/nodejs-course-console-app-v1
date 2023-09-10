e5eaab1c75d4c13aca630c0d29963b37d89389ed
RidgeA <antongriadchenko@gmail.com>
2023-09-10 20:16:14 +0300

    write cache to dir

--- services/geocode-cache.js
+++ services/geocode-cache.js
@@ -1,11 +1,26 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+const CACHE_FILE = "./geocode-cache.json";
+
 export class GeocodeCache {
   #geocodeProvider;
+  #cacheDir;
 
-  constructor(geocodeProvider) {
+  constructor(geocodeProvider, cacheDir) {
     this.#geocodeProvider = geocodeProvider;
+    this.#cacheDir = cacheDir;
   }
 
   async getCoordinatesByCityName(name) {
-    return await this.#geocodeProvider.getCoordinatesByCityName(name);
+    const cache = {};
+    const result = await this.#geocodeProvider.getCoordinatesByCityName(name);
+    cache[name] = result;
+    await this.#writeToCache(cache);
+    return result;
+  }
+
+  async #writeToCache(cache) {
+    const json = JSON.stringify(cache);
+    await fs.writeFile(path.join(this.#cacheDir, CACHE_FILE), json);
   }
 }
