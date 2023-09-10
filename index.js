b95149d687d411bff98068a4c4429e9a2b19658f
RidgeA <antongriadchenko@gmail.com>
2023-09-10 22:03:20 +0300

    use GeocodeCache

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import { getCurrentWeather } from "./command/current.js";
 import { OpenMeteo } from "./services/open-meteo.js";
 import { OpenMeteoGeocoding } from "./services/open-meteo-geocoding.js";
 import { HttpClient } from "./lib/http-client.js";
+import { GeocodeCache } from "./services/geocode-cache.js";
+import path from "node:path";
+import os from "node:os";
 
 console.log("The Weather CLI App");
 
@@ -10,15 +13,15 @@ const httpClient = new HttpClient();
 const weatherProvider = new OpenMeteo(httpClient);
 const geocodingProvider = new OpenMeteoGeocoding(httpClient);
 
+const appDataDir = resolveAppDataDir();
+
+const geocodeCache = new GeocodeCache(geocodingProvider, appDataDir);
+
 try {
   const command = parseCommand(process.argv.slice(2));
 
   if (command.command === "current") {
-    await getCurrentWeather(
-      weatherProvider,
-      geocodingProvider,
-      command.options,
-    );
+    await getCurrentWeather(weatherProvider, geocodeCache, command.options);
   } else {
     throw new Error(`Unknown command: ${command.command}`);
   }
@@ -32,4 +35,11 @@ try {
 
   console.error(msg);
   process.exit(1);
+}
+
+function resolveAppDataDir() {
+  if (process.env.WEATHER_APPLICATION_DATA) {
+    return process.env.WEATHER_APPLICATION_DATA;
+  }
+  return path.join(os.homedir(), ".weather-cli");
 }
